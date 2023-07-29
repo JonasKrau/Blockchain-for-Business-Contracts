90cb223ae15b752bf75dd9e414de33e5072167f7
jkrause96 <krause@tu-berlin.de>
2023-07-29 14:06:30 +0200

    Hide Input Values

--- Application.py
+++ Application.py
@@ -1,6 +1,14 @@
 import tkinter as tk
 from tkinter import ttk, filedialog
 
+def toggle_show_hide(entry_widget, lock_button):
+    if entry_widget.cget("show") == "":
+        entry_widget.configure(show="*")
+        lock_button.config(text="Verbergen/Anzeigen")
+    else:
+        entry_widget.configure(show="")
+        lock_button.config(text="Verbergen/Anzeigen")
+
 def submit_text():
     # Vertrag aus Datei laden
     file_path = filedialog.askopenfilename(filetypes=[("Textdateien", "*.txt")])
@@ -30,8 +38,7 @@ button_text_color = 'white'  # Button-Textfarbe
 # Styles für das Design definieren
 style = ttk.Style()
 style.configure('TLabel', font=('Arial', 12), foreground=text_color, background=bg_color, wraplength=600, justify='left')
-style.configure('TEntry', font=('Arial', 12), background=bg_color, width=50)
-style.configure('TEntry.TEntry', foreground=text_color, show='*')  # Passwort-Eingabe mit Sternchen
+style.configure('TEntry', font=('Arial', 12), background=bg_color, width=50, show="*")  # Eingabe mit Verbergen
 style.map('TEntry', foreground=[('focus', text_color)])  # Textfarbe für Eingabe mit Focus (ohne Placeholder)
 
 # Label für private Schlüssel der Vertragspartei 1
@@ -39,53 +46,31 @@ private_key_label_1 = ttk.Label(root, text="Privater Schlüssel Vertragspartei 1
 private_key_label_1.pack(pady=5)
 
 # Eingabefeld für privaten Schlüssel der Vertragspartei 1
-private_key_placeholder_1 = "Privater Schlüssel Vertragspartei 1"
-private_key_entry_1 = ttk.Entry(root, width=50, show='*')
-private_key_entry_1.insert(0, private_key_placeholder_1)
+private_key_entry_1 = ttk.Entry(root)
 private_key_entry_1.pack(pady=5)
 
-def on_click_entry_1(event):
-    if private_key_entry_1.get() == private_key_placeholder_1:
-        private_key_entry_1.delete(0, tk.END)
-        private_key_entry_1.configure(style='TEntry.TEntry')
-
-def on_leave_entry_1(event):
-    if private_key_entry_1.get() == "":
-        private_key_entry_1.insert(0, private_key_placeholder_1)
-        private_key_entry_1.configure(style='TEntry')
-
-private_key_entry_1.bind('<FocusIn>', on_click_entry_1)
-private_key_entry_1.bind('<FocusOut>', on_leave_entry_1)
+# Lock-Button für privaten Schlüssel der Vertragspartei 1
+lock_button_1 = ttk.Button(root, text="Verbergen", command=lambda: toggle_show_hide(private_key_entry_1, lock_button_1))
+lock_button_1.pack()
 
 # Label für private Schlüssel der Vertragspartei 2
 private_key_label_2 = ttk.Label(root, text="Privater Schlüssel Vertragspartei 2:")
 private_key_label_2.pack(pady=5)
 
 # Eingabefeld für privaten Schlüssel der Vertragspartei 2
-private_key_placeholder_2 = "Privater Schlüssel Vertragspartei 2"
-private_key_entry_2 = ttk.Entry(root, width=50, show='*')
-private_key_entry_2.insert(0, private_key_placeholder_2)
+private_key_entry_2 = ttk.Entry(root)
 private_key_entry_2.pack(pady=5)
 
-def on_click_entry_2(event):
-    if private_key_entry_2.get() == private_key_placeholder_2:
-        private_key_entry_2.delete(0, tk.END)
-        private_key_entry_2.configure(style='TEntry.TEntry')
-
-def on_leave_entry_2(event):
-    if private_key_entry_2.get() == "":
-        private_key_entry_2.insert(0, private_key_placeholder_2)
-        private_key_entry_2.configure(style='TEntry')
-
-private_key_entry_2.bind('<FocusIn>', on_click_entry_2)
-private_key_entry_2.bind('<FocusOut>', on_leave_entry_2)
+# Lock-Button für privaten Schlüssel der Vertragspartei 2
+lock_button_2 = ttk.Button(root, text="Verbergen", command=lambda: toggle_show_hide(private_key_entry_2, lock_button_2))
+lock_button_2.pack()
 
 # Eingabefeld für Dateiauswahl erstellen
 file_button = ttk.Button(root, text="Vertrag auswählen", command=submit_text)
 file_button.pack(pady=10)
 
 # Button erstellen
-submit_button = ttk.Button(root, text="Vertrag speichern", command=submit_text)
+submit_button = ttk.Button(root, text="Vertrag signieren", command=submit_text)
 submit_button.pack(pady=10)
 
 # Label erstellen, um den eingegebenen Text als Vertrag anzuzeigen
