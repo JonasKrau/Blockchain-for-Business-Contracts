9310db23fb42a28382cccd8f88733cd7364130eb
jkrause96 <krause@tu-berlin.de>
2023-08-08 13:09:42 +0200

    Query function included

--- SC/Contract.sol
+++ SC/Contract.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract BusinessContract {
+contract BusinessContractV2 {
     // Datenstruktur für einen Vertrag
     struct Contract {
         string encryptedSignature;
@@ -12,6 +12,9 @@ contract BusinessContract {
     // Mapping von contractId zu Contract
     mapping (string => Contract) private contracts;
 
+    // Event, das geloggt wird, wenn ein Vertrag gespeichert wird
+    event ContractStored(string contractId, string encryptedSignature, string pubkey1, string pubkey2);
+
     // Funktion zum Speichern eines Vertrags
     function storeContract(string memory contractId, string memory encryptedSignature, string memory pubkey1, string memory pubkey2) public {
         Contract memory newContract = Contract({
@@ -19,7 +22,16 @@ contract BusinessContract {
             pubkey1: pubkey1,
             pubkey2: pubkey2
         });
-        
+
         contracts[contractId] = newContract;
+
+        // Loggt das Ereignis
+        emit ContractStored(contractId, encryptedSignature, pubkey1, pubkey2);
+    }
+
+    // Funktion zum Abrufen der Daten eines Vertrags anhand der contractId
+    function getContract(string memory contractId) public view returns (string memory encryptedSignature, string memory pubkey1, string memory pubkey2) {
+        Contract memory contractData = contracts[contractId];
+        return (contractData.encryptedSignature, contractData.pubkey1, contractData.pubkey2);
     }
 }
