dfabcf0bd55efcd7926c0b6d918a48623f1bb714
Jonas Krause <103564990+JonasKrau@users.noreply.github.com>
2023-08-23 14:14:30 +0200

    Comments in English

--- Lookup/DisplayAll.py
+++ Lookup/DisplayAll.py
@@ -1,25 +1,26 @@
 import tkinter as tk
 
+
 def display_contract(decrypted_contract):
-    # Erstellen des Hauptfensters
+    # Create main window
     root = tk.Tk()
     root.option_add('*Font', 'Helvetica 20 bold')
     root.title('Decrypted Contract')
 
-    # Erstellen eines Labels, um die erfolgreiche Verifizierungsnachricht anzuzeigen
+    # Create a label to indicate the successful verification message
     verification_label = tk.Label(root, text="Signatures have been verified successfully!", wraplength=800, fg="green")
     verification_label.pack()
 
-    # Erstellen eines Labels für die Überschrift "Decrypted Contract:"
+    # Creating a label for the heading "Decrypted Contract:"
     contract_heading = tk.Label(root, text="Decrypted Contract:", font='Helvetica 18')
     contract_heading.pack()
 
-    # Erstellen eines Textwidgets, um den Vertrag anzuzeigen
+    # Create a text widget to display the contract
     text_widget = tk.Text(root, wrap=tk.WORD, font='Helvetica 14')
     text_widget.pack(expand=tk.YES, fill=tk.BOTH)
 
-    # Hinzufügen des entschlüsselten Vertrags zum Textwidget
+    # Adding the decrypted contract to the text widget
     text_widget.insert(tk.END, decrypted_contract)
 
-    # Hauptereignisschleife ausführen
+    # Run main event loop
     root.mainloop()
